bed2214728377675025cf728c744913de8ee8349
Julio Romero <julio_ale21@hotmail.com>
2021-04-16 20:07:44 -0300

    chore: added eslint + prettier config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 100,
+        singleQuote: false,
+        trailingComma: "all",
+        semi: true,
+        endOfLine: "auto",
+        bracketSpacing: true,
+        arrowParens: "always",
+      },
+    ],
+    "react/prop-types": 0,
+    "no-console": "warn",
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        callbacksLast: true,
+        shorthandFirst: true,
+        noSortAlphabetically: false,
+        reservedFirst: true,
+      },
+    ],
+    "padding-line-between-statements": [
+      "error",
+      { blankLine: "always", prev: "*", next: "return" },
+      { blankLine: "always", prev: ["const", "let", "var"], next: "*" },
+      { blankLine: "any", prev: ["const", "let", "var"], next: ["const", "let"] },
+    ],
+  },
+};
